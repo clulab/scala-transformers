60a696b6bc4f476e7c7878ced59e6d66357ad780
Mihai Surdeanu <surdeanu@gmail.com>
2022-10-30 12:15:01 -0700

    draft of linear layer complete

--- encoder/src/main/scala/org/clulab/scala_transformers/encoder/LinearLayer.scala
+++ encoder/src/main/scala/org/clulab/scala_transformers/encoder/LinearLayer.scala
@@ -5,7 +5,11 @@ import java.io.File
 import scala.collection.mutable.ArrayBuffer
 
 /** Implements one linear layer */
-class LinearLayer (name: String, weights: DenseMatrix[Float], biases: Option[DenseVector[Float]], labels: Option[Array[String]]){
+class LinearLayer (name: String, 
+  weights: DenseMatrix[Float], // dimensions (hidden state size x labels size)
+  biases: Option[DenseVector[Float]], // column vector with length = labels size
+  labels: Option[Array[String]]){
+
   /** Forward pass for a single sentence */
   def forward(inputSentence: DenseMatrix[Float]): DenseMatrix[Float] = {
     val batch = new Array[DenseMatrix[Float]](1)
@@ -13,11 +17,51 @@ class LinearLayer (name: String, weights: DenseMatrix[Float], biases: Option[Den
     forward(batch)(0)
   }
 
-  /** Forward pass for a batch of sentences */
+  /** 
+   * Forward pass for a batch of sentences 
+   * @param inputBatch Each matrix in the batch has dimensions (sentence size x hidden state size)
+   * @return Each output matrix has dimensions (sentence size x labels size)
+   */
   def forward(inputBatch: Array[DenseMatrix[Float]]): Array[DenseMatrix[Float]] = {
-    // AICI
-    null
+    val outputBatch = new ArrayBuffer[DenseMatrix[Float]]()
+    for(input <- inputBatch) {
+      val output = input * weights
+      for(b <- biases) output(*, ::) :+= b
+      outputBatch += output
+    }
+    outputBatch.toArray
   }  
+
+  /** Predict the top label per token */
+  def predict(inputSentence: DenseMatrix[Float]): Array[String] = {
+    val batch = new Array[DenseMatrix[Float]](1)
+    batch(0) = inputSentence
+    predict(batch)(0)
+  }
+
+  /** Predict the top label for each token in each sentence in the batch */
+  def predict(inputBatch: Array[DenseMatrix[Float]]): Array[Array[String]] = {
+    if(labels.isEmpty) {
+      throw new RuntimeException("ERROR: can't predict without labels!")
+    }
+
+    // predict best label per (subword) token
+    val logits = forward(inputBatch)
+    val outputBatch = new ArrayBuffer[Array[String]]()
+    for(logitsPerSentence <- logits) {
+      val predLabels = new ArrayBuffer[String]
+      // one token per row; pick argmax per token
+      for(i <- 0 until logitsPerSentence.rows) {
+        val row = logitsPerSentence(i, ::)
+        val bestIndex = argmax(row.t)
+        val bestLabel = labels.get(bestIndex)
+        predLabels += bestLabel
+      }
+      outputBatch += predLabels.toArray
+    }
+
+    outputBatch.toArray
+  }
 }
 
 object LinearLayer {
@@ -59,6 +103,7 @@ object LinearLayer {
       values += row.toArray
     }
     source.close()
+    // dimensions: rows = hidden state size, columns = labels' count
     BreezeUtils.mkRowMatrix(values.toArray).t
   }
 
@@ -72,6 +117,7 @@ object LinearLayer {
       }
     }
     source.close()
+    // the bias is a column vector
     DenseVector(values.toArray)
   }
 
