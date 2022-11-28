4b422560ad70a47ed4092f12e9a0b63c53f19988
Mihai Surdeanu <surdeanu@gmail.com>
2022-11-28 12:50:50 -0700

    bug fix; generating correct token head positions for deps labels

--- encoder/src/main/python/data_wrangling.py
+++ encoder/src/main/python/data_wrangling.py
@@ -31,6 +31,48 @@ def align_labels(word_ids, labels, label_to_index):
         previous_word_id = word_id
     
     return label_ids
+
+# map word-level head positions to subword tokens
+def align_head_positions(word_ids, word_heads):
+  # map from word positions to first-in-word token positions 
+  #print(f'word_ids = {word_ids}')
+  #print(f'word_heads = {word_heads}')
+  word_to_token_map = {}
+  previous_word_id = None
+  for i in range(0, len(word_ids)):
+    # we are seeing the first token in a word
+    if(word_ids[i] != None and word_ids[i] != previous_word_id):
+      word_to_token_map[word_ids[i]] = i
+    previous_word_id = word_ids[i]
+
+  #print(f'word_to_token_map = {word_to_token_map}')
+
+  # stores the position of the token that is the head for each word
+  token_head_positions = []
+  previous_word_id = None
+  for word_id in word_ids:
+      # we are inside an existing word or looking at [CLS]
+      if word_id is None or word_id == previous_word_id:
+          token_head_positions.append(ignore_index)
+      # beginning of a word whose head is root (-1)
+      elif word_heads[word_id] == -1: 
+          token_head_positions.append(-1)
+      # beginning of a word whose head is not root
+      else:
+          # get head position for corresponding word
+          #print(f'\tword_id = {word_id}')
+          #print(f'\tword_heads[word_id] = {word_heads[word_id]}')
+          token_head_positions.append(word_to_token_map[word_heads[word_id]])
+      # remember this word id
+      previous_word_id = word_id
+
+  return token_head_positions
+
+def make_empty_list(length, value):
+  l = []
+  for i in range(0, length):
+    l.append(value)
+  return l
             
 # build a set of labels in the dataset            
 def read_label_set(fn):
@@ -47,11 +89,12 @@ def read_label_set(fn):
 # converts a two-column file in the basic MTL format ("word \t label") into a dataframe
 def read_dataframe(fn, label_to_index, task_id, tokenizer):
     # now build the actual dataframe for this dataset
-    data = {'words': [], 'str_labels': [], 'input_ids': [], 'word_ids': [], 'labels': [], 'task_ids': []}
+    data = {'words': [], 'str_labels': [], 'input_ids': [], 'word_ids': [], 'labels': [], 'head_positions': [], 'task_ids': []}
     with open(fn) as f:
         sent_words = []
         sent_labels = [] 
         head_positions = []
+
         for index, line in tqdm(enumerate(f)):
             line = line.strip()
             tokens = line.split()
@@ -66,13 +109,29 @@ def read_dataframe(fn, label_to_index, task_id, tokenizer):
                 
                 # map labels to the first token in each word
                 token_labels = align_labels(word_ids, sent_labels, label_to_index)
+
+                # if present, map head offsets to the first token in each word
+                token_head_positions = align_head_positions(word_ids, head_positions) if len(head_positions) > 0 else None
                 
                 data['input_ids'].append(token_ids)
                 data['word_ids'].append(word_ids)
                 data['labels'].append(token_labels)
+                if token_head_positions != None:
+                  data['head_positions'].append(token_head_positions)
+                else:
+                  data['head_positions'].append(make_empty_list(len(word_ids), ignore_index))
                 data['task_ids'].append(task_id)
+
+                #if task_id == 4:
+                #  print(f'sent_words = {sent_words}')
+                #  print(f'input_ids = {token_ids}')
+                #  print(f'word_ids = {word_ids}')
+                #  print(f'head_positions = {head_positions}')
+                #  print(f'token_head_positions = {token_head_positions}')                  
+
                 sent_words = []
                 sent_labels = [] 
+                head_positions = []
             else:
                 sent_words.append(tokens[0]) # tokens 
                 sent_labels.append(tokens[1]) # labels
--- encoder/src/main/python/trainer.py
+++ encoder/src/main/python/trainer.py
@@ -29,7 +29,8 @@ def main():
   pos_task = Task(1, "POS", "data/pos/train.txt", "data/pos/dev.txt", "data/pos/test.txt", tokenizer)
   chunk_task = Task(2, "Chunking", "data/chunking/train.txt", "data/chunking/test.txt", "data/chunking/test.txt", tokenizer)
   deph_task = Task(3, "Deps Head", "data/deps-wsj/train.heads", "data/deps-wsj/dev.heads", "data/deps-wsj/test.heads", tokenizer)
-  tasks = [ner_task, pos_task, chunk_task, deph_task]
+  depl_task = Task(4, "Deps Label", "data/deps-wsj/train.labels", "data/deps-wsj/dev.labels", "data/deps-wsj/test.labels", tokenizer)
+  tasks = [ner_task, pos_task, chunk_task, deph_task, depl_task]
 
   # our own token classifier
   model= TokenClassificationModel.from_pretrained(cf.transformer_name, config=config, ignore_mismatched_sizes=True).add_heads(tasks)
@@ -37,9 +38,24 @@ def main():
 
   # create the formal train/validation/test HF dataset
   ds = DatasetDict()
-  ds['train'] = Dataset.from_pandas(pd.concat([ner_task.train_df, pos_task.train_df, chunk_task.train_df, deph_task.train_df]))
-  ds['validation'] = Dataset.from_pandas(pd.concat([ner_task.dev_df, pos_task.dev_df, chunk_task.dev_df, deph_task.dev_df]))
-  ds['test'] = Dataset.from_pandas(pd.concat([ner_task.test_df, pos_task.test_df, chunk_task.test_df, deph_task.test_df]))
+  ds['train'] = Dataset.from_pandas(pd.concat([
+    ner_task.train_df, 
+    pos_task.train_df, 
+    chunk_task.train_df, 
+    deph_task.train_df, 
+    depl_task.train_df]))
+  ds['validation'] = Dataset.from_pandas(pd.concat([
+    ner_task.dev_df, 
+    pos_task.dev_df, 
+    chunk_task.dev_df, 
+    deph_task.dev_df,
+    depl_task.dev_df]))
+  ds['test'] = Dataset.from_pandas(pd.concat([
+    ner_task.test_df, 
+    pos_task.test_df, 
+    chunk_task.test_df, 
+    deph_task.test_df,
+    depl_task.test_df]))
 
   data_collator = DataCollatorForTokenClassification(tokenizer)
   last_checkpoint = None
@@ -85,12 +101,13 @@ def main():
     pos_acc = evaluate(trainer, pos_task, "POS")
     chunk_acc = evaluate(trainer, chunk_task, "Chunking")
     deph_acc = evaluate(trainer, deph_task, "Deps Head")
-    macro_acc = (ner_acc + pos_acc + chunk_acc + deph_acc)/4
+    depl_acc = evaluate(trainer, depl_task, "Deps Label")
+    macro_acc = (ner_acc + pos_acc + chunk_acc + deph_acc + depl_acc)/5
     print(f'Dev macro accuracy for epoch {epoch}: {macro_acc}')
     model.training_mode = True
 
     # keep track of validation scores per epoch
-    save_stats("epoch_stats.txt", tasks, [ner_acc, pos_acc, chunk_acc, deph_acc], macro_acc, epoch)
+    save_stats("epoch_stats.txt", tasks, [ner_acc, pos_acc, chunk_acc, deph_acc, depl_acc], macro_acc, epoch)
 
     # save the transformer encoder + the head for each task
     last_checkpoint = training_args.output_dir + '/mtl_model_epoch' + str(epoch)
