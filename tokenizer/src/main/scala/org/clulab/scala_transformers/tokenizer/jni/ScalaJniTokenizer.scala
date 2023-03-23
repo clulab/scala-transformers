f170acc77148de57c25ce576c737136b3de1f040
Keith Alcock <github@keithalcock.com>
2023-03-23 15:38:02 -0700

    Implement the addPrefixSpace in Scala

--- tokenizer/src/main/scala/org/clulab/scala_transformers/tokenizer/jni/ScalaJniTokenizer.scala
+++ tokenizer/src/main/scala/org/clulab/scala_transformers/tokenizer/jni/ScalaJniTokenizer.scala
@@ -7,15 +7,18 @@ import org.clulab.scala_transformers.tokenizer.Tokenizer
 import scala.collection.mutable.{HashMap => MutableHashMap}
 import scala.ref.WeakReference
 
-class ScalaJniTokenizer(name: String) extends Tokenizer(name) {
-  val tokenizerId = JavaJniTokenizer.create(name)
+class ScalaJniTokenizer(name: String, addPrefixSpace: Boolean = false) extends Tokenizer(name) {
+  val tokenizerId: Long = JavaJniTokenizer.create(name)
 
   override def finalize: Unit = {
     JavaJniTokenizer.destroy(tokenizerId)
   }
 
   override def tokenize(words: Array[String]): Tokenization = {
-    val javaJniTokenization = JavaJniTokenizer.tokenize(tokenizerId, words)
+    val tokenizedWords =
+        if (addPrefixSpace) words.map(" " + _)
+        else words
+    val javaJniTokenization = JavaJniTokenizer.tokenize(tokenizerId, tokenizedWords)
 
     Tokenization(
       javaJniTokenization.tokenIds,
@@ -26,19 +29,20 @@ class ScalaJniTokenizer(name: String) extends Tokenizer(name) {
 }
 
 object ScalaJniTokenizer {
-  val rustLibrary = LibraryLoader.load("rust_tokenizer")
-  val map = new MutableHashMap[String, WeakReference[ScalaJniTokenizer]]()
+  val rustLibrary: Unit = LibraryLoader.load("rust_tokenizer")
+  val map = new MutableHashMap[(String, Boolean), WeakReference[ScalaJniTokenizer]]()
 
-  def apply(name: String): ScalaJniTokenizer = synchronized {
+  def apply(name: String, addPrefixSpace: Boolean = false): ScalaJniTokenizer = synchronized {
+    val key = (name, addPrefixSpace)
     // If the key is known and the weak reference is valid, then the result is
     // Some(scalaTokenizer) with a strong reference that will remain valid.
-    val scalaTokenizerOpt = map.get(name).flatMap(_.get)
+    val scalaTokenizerOpt = map.get(key).flatMap(_.get)
 
     if (scalaTokenizerOpt.isDefined)
       scalaTokenizerOpt.get
     else {
-      val scalaTokenizer = new ScalaJniTokenizer(name)
-      map(name) = WeakReference(scalaTokenizer)
+      val scalaTokenizer = new ScalaJniTokenizer(name, addPrefixSpace)
+      map(key) = WeakReference(scalaTokenizer)
       scalaTokenizer
     }
   }
