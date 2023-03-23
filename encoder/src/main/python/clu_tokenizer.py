3433842586df4b4f74ec8e2190a097b6037cc403
Keith Alcock <github@keithalcock.com>
2023-03-23 16:22:55 -0700

    Try out some other tokenizers

--- encoder/src/main/python/clu_tokenizer.py
+++ encoder/src/main/python/clu_tokenizer.py
@@ -5,8 +5,8 @@ from transformers import AutoTokenizer
 
 class CluTokenizer:
     @classmethod
-    def get_pretrained(cls) -> AutoTokenizer:
+    def get_pretrained(cls, name: str = parameters.transformer_name, add_prefix_space: bool = False) -> AutoTokenizer:
         # which transformer to use
-        print(f"Loading tokenizer named \"{parameters.transformer_name}\"...")
-        tokenizer = AutoTokenizer.from_pretrained(parameters.transformer_name, model_input_names=[names.INPUT_IDS, "token_type_ids", "attention_mask"])
+        print(f"Loading tokenizer named \"{name}\" with add_prefix_space={add_prefix_space}...")
+        tokenizer = AutoTokenizer.from_pretrained(name, model_input_names=[names.INPUT_IDS, "token_type_ids", "attention_mask"])
         return tokenizer
---
+++ encoder/src/main/python/test_clu_tokenizer.py
@@ -0,0 +1,16 @@
+
+from clu_tokenizer import CluTokenizer
+
+def test_get_pretrained() -> None:
+    tokenizer = CluTokenizer.get_pretrained()
+
+    tokenizer = CluTokenizer.get_pretrained("bert-base-cased")
+    tokenizer = CluTokenizer.get_pretrained("xlm-roberta-base")
+    tokenizer = CluTokenizer.get_pretrained("distilbert-base-cased")
+    tokenizer = CluTokenizer.get_pretrained("roberta-base", add_prefix_space=True)
+    tokenizer = CluTokenizer.get_pretrained("google/bert_uncased_L-4_H-512_A-8")
+    tokenizer = CluTokenizer.get_pretrained("microsoft/deberta-v3-base")
+
+
+if __name__ == "__main__":
+    test_get_pretrained()
