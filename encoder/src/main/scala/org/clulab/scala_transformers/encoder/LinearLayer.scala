cddcc447d8e0efdd67795aab09686b4846fa34f8
Mihai Surdeanu <surdeanu@gmail.com>
2023-04-04 10:44:52 -0700

    dual prediction in Scala

--- encoder/src/main/scala/org/clulab/scala_transformers/encoder/LinearLayer.scala
+++ encoder/src/main/scala/org/clulab/scala_transformers/encoder/LinearLayer.scala
@@ -58,36 +58,66 @@ class LinearLayer(
     else predictPrimal(inputBatch)
   }
 
+  def concatenateModifiersAndHeads(
+    sentenceHiddenStates: DenseMatrix[Float], 
+    headRelativePositions: Array[Int]): DenseMatrix[Float] = {
+
+    // this matrix concatenates the hidden states of modifier + corresponding head
+    // rows = number of tokens in the sentence; cols = hidden state size
+    val concatMatrix = DenseMatrix.zeros[Float](rows = sentenceHiddenStates.rows, cols = 2 * sentenceHiddenStates.cols)
+
+    // traverse all modifiers
+    for(i <- 0 until sentenceHiddenStates.rows) {
+      val modHiddenState = sentenceHiddenStates(i, ::)
+      // what is the absolute position of the head token in the sentence?
+      val rawHeadAbsPos = i + headRelativePositions(i)
+      val headAbsolutePosition = 
+        if(rawHeadAbsPos >= 0 && rawHeadAbsPos < sentenceHiddenStates.rows) rawHeadAbsPos
+        else i // if the absolute position is invalid (e.g., root node or incorrect prediction) duplicate the mod embedding
+      val headHiddenState = sentenceHiddenStates(headAbsolutePosition, ::)
+
+      // vector concatenation in Breeze operates over vertical vectors, hence the transposing here
+      val concatState = DenseVector.vertcat(modHiddenState.t, headHiddenState.t).t
+
+      // row i in the concatenated matrix contains the embedding of modifier i and its head
+      concatMatrix(i, ::) :+= concatState
+    }
+    
+    concatMatrix
+  }
+
+  /** Predict the top label for each combination of modifier token and corresponding head token */
   def predictDual(inputBatch: Array[DenseMatrix[Float]], 
                   batchHeads: Option[Array[Array[Int]]] = None,
                   batchMasks: Option[Array[Array[Boolean]]] = None): Array[Array[String]] = {
     assert(batchHeads.isDefined)
     assert(batchMasks.isDefined)
+    val indexToLabel = labelsOpt.getOrElse(throw new RuntimeException("ERROR: can't predict without labels!"))
 
     val outputBatch = new Array[Array[String]](inputBatch.length)
+
+    // we process one sentence at a time because the dual setting makes it harder to batch
     for(i <- inputBatch.indices) {
-      val labels = new Array[String](batchHeads.get(i).length)
       val input = inputBatch(i)
       val heads = batchHeads.get(i)
       val masks = batchMasks.get(i)
-      
-      for(mod <- heads.indices) {
-        val head = heads(mod)
-        if(masks(mod)) {
-          labels(mod) = "nil"
-        } else {
-          // TODO: dual task!
-          // concat mod then head
 
-          // println(input.dimensions)
+      // generate a matrix that is twice as wide to concatenate the embeddings of the mod + head
+      val concatInput = concatenateModifiersAndHeads(input, heads)
 
-          labels(mod) = "TODO"
-        }
+      // get the logits for the current sentence produced by this linear layer
+      val logitsPerSentence = forward(Array(concatInput))(0)
+
+      // one token per row; pick argmax per token
+      val bestLabels = Range(0, logitsPerSentence.rows).map { i =>
+        val row = logitsPerSentence(i, ::) // picks line i from a 2D matrix
+        val bestIndex = argmax(row.t)
+        indexToLabel(bestIndex)
       }
-      
-      // println("token labels: " + labels.mkString(", "))
-      outputBatch(i) = labels
+
+      outputBatch(i) = bestLabels.toArray
     }
+
     outputBatch                    
   }
 
@@ -98,7 +128,7 @@ class LinearLayer(
     val outputBatch = logits.map { logitsPerSentence =>
       // one token per row; pick argmax per token
       val bestLabels = Range(0, logitsPerSentence.rows).map { i =>
-        val row = logitsPerSentence(i, ::)
+        val row = logitsPerSentence(i, ::) // picks line i from a 2D matrix
         val bestIndex = argmax(row.t)
 
         labels(bestIndex)
