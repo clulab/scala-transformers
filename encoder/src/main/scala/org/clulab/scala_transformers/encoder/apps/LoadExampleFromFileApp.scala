e7cb6299c296ce3199bbc59b931198df0731be7c
Keith Alcock <github@keithalcock.com>
2023-04-06 08:39:51 -0700

    Get other programs to work

--- encoder/src/main/scala/org/clulab/scala_transformers/encoder/apps/LoadExampleFromFileApp.scala
+++ encoder/src/main/scala/org/clulab/scala_transformers/encoder/apps/LoadExampleFromFileApp.scala
@@ -1,17 +1,18 @@
 package org.clulab.scala_transformers.encoder.apps
 
-import org.clulab.scala_transformers.encoder.{Encoder, TokenClassifierLayout, TokenClassifierFactoryFromFiles, TokenClassifier}
+import org.clulab.scala_transformers.encoder.{Encoder, TokenClassifierFactoryFromFiles, TokenClassifierLayout}
 import org.clulab.scala_transformers.tokenizer.LongTokenization
 import org.clulab.scala_transformers.tokenizer.jni.ScalaJniTokenizer
 
 object LoadExampleFromFileApp extends App {
   val baseName = args.lift(0).getOrElse("../tcmodel")
-  val modelLayout = new TokenClassifierLayout(baseName)
+  val tokenClassifierLayout = new TokenClassifierLayout(baseName)
+  val tokenClassifierFactory = new TokenClassifierFactoryFromFiles(tokenClassifierLayout)
   val words = Array("EU", "rejects", "German", "call", "to", "boycott", "British", "lamb", ".")
-  val tokenizer = ScalaJniTokenizer(modelLayout.name)
+  val tokenizer = ScalaJniTokenizer(tokenClassifierFactory.name)
   val tokenization = tokenizer.tokenize(words)
   val inputIds = LongTokenization(tokenization).tokenIds
-  val encoder = Encoder.fromFile(modelLayout.onnx)
+  val encoder = Encoder.fromFile(tokenClassifierLayout.onnx)
   val encOutput = encoder.forward(inputIds)
 
   println(encOutput)
--- encoder/src/main/scala/org/clulab/scala_transformers/encoder/apps/LoadExampleFromResourceApp.scala
+++ encoder/src/main/scala/org/clulab/scala_transformers/encoder/apps/LoadExampleFromResourceApp.scala
@@ -1,17 +1,18 @@
 package org.clulab.scala_transformers.encoder.apps
 
-import org.clulab.scala_transformers.encoder.{Encoder, TokenClassifierLayout, TokenClassifier}
+import org.clulab.scala_transformers.encoder.{Encoder, TokenClassifierFactoryFromResources, TokenClassifierLayout}
 import org.clulab.scala_transformers.tokenizer.LongTokenization
 import org.clulab.scala_transformers.tokenizer.jni.ScalaJniTokenizer
 
 object LoadExampleFromResourceApp extends App {
-  val baseName = args.lift(0).getOrElse("../tcmodel")
-  val modelLayout = new TokenClassifierLayout(baseName)
+  val baseName = "/org/clulab/scala_transformers/models/roberta_base_mtl/avg_export"
+  val tokenClassifierLayout = new TokenClassifierLayout(baseName)
+  val tokenClassifierFactory = new TokenClassifierFactoryFromResources(tokenClassifierLayout)
   val words = Array("EU", "rejects", "German", "call", "to", "boycott", "British", "lamb", ".")
-  val tokenizer = ScalaJniTokenizer(modelLayout.name)
+  val tokenizer = ScalaJniTokenizer(tokenClassifierFactory.name)
   val tokenization = tokenizer.tokenize(words)
   val inputIds = LongTokenization(tokenization).tokenIds
-  val encoder = Encoder.fromResource(modelLayout.onnx)
+  val encoder = Encoder.fromResource(tokenClassifierLayout.onnx)
   val encOutput = encoder.forward(inputIds)
 
   println(encOutput)
