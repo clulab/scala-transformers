caf6edbb21bb7ec6328c385849ba170bae463e6f
Keith Alcock <github@keithalcock.com>
2023-04-05 17:04:08 -0700

    Compile it

--- encoder/src/test/scala/org/clulab/scala_transformers/encoder/timer/TokenClassifierTimerApp.scala
+++ encoder/src/test/scala/org/clulab/scala_transformers/encoder/timer/TokenClassifierTimerApp.scala
@@ -81,7 +81,7 @@ object TokenClassifierTimerApp extends App {
     }
   }
 
-  val tokenClassifier = new TimedTokenClassifier(TokenClassifier("../tcmodel"))
+  val tokenClassifier = new TimedTokenClassifier(TokenClassifier.fromFiles("../tcmodel"))
   val lines = {
     val source = Source.fromFile(fileName)
     val lines = source.getLines.toArray
