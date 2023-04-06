d1933985613f894c01dd8fd28d98d1b71db16e95
Keith Alcock <github@keithalcock.com>
2023-04-06 13:59:51 -0700

    Handle warnings

--- encoder/src/main/scala/org/clulab/scala_transformers/encoder/apps/TokenClassifierExampleApp.scala
+++ encoder/src/main/scala/org/clulab/scala_transformers/encoder/apps/TokenClassifierExampleApp.scala
@@ -10,10 +10,10 @@ import org.clulab.scala_transformers.tokenizer.LongTokenization
 */
 
 object TokenClassifierExampleApp extends App {
-   val tokenClassifier = TokenClassifier.fromFiles("../roberta-base-mtl/avg_export")
+  val tokenClassifier = TokenClassifier.fromFiles("../roberta-base-mtl/avg_export")
 //  val tokenClassifier = TokenClassifier.fromResources("/org/clulab/scala_transformers/models/roberta_base_mtl/avg_export")
 
-  val words = Array("EU", "rejects", "German", "call", "to", "boycott", "British", "lamb", ".")
+  val words = Seq("EU", "rejects", "German", "call", "to", "boycott", "British", "lamb", ".")
   println(s"Words: ${words.mkString(", ")}")
   val allLabels = tokenClassifier.predict(words)
   for (labels <- allLabels) {
