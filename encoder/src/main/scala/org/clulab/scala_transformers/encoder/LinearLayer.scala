1f0ed3f3f241906a45ce9f75ca43cb77b09aeebf
Keith Alcock <github@keithalcock.com>
2024-01-02 21:51:09 -0700

    Change from vertcat to horcat

--- encoder/src/main/scala/org/clulab/scala_transformers/encoder/LinearLayer.scala
+++ encoder/src/main/scala/org/clulab/scala_transformers/encoder/LinearLayer.scala
@@ -98,7 +98,7 @@ class LinearLayer(
 
       // row i in the concatenated matrix contains the embedding of modifier i and its head
       if(USE_CONCAT) {
-        val concatState = Math.vertcat(modHiddenState, headHiddenState)
+        val concatState = Math.horcat(modHiddenState, headHiddenState)
         Math.inplaceMatrixAddition(concatMatrix, i, concatState)
       } else {
         Math.inplaceMatrixAddition(concatMatrix, i, modHiddenState)
